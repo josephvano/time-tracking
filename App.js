fe0db7639708b69cdf66fabbef0c299b713dc3fd
Joey Vano <joseph@vano.io>
2020-01-25 16:44:15 -0500

    refactor(timers) introduce array of timers
    * map over timers to create `EditableTimers`

--- App.js
+++ App.js
@@ -1,34 +1,55 @@
 import React                                from 'react';
 import {StyleSheet, ScrollView, Text, View} from 'react-native';
+import uuiidv4                              from 'uuid/v4';
 import EditableTimer                        from './components/EditableTimer';
 import ToggleableTimerForm                  from './components/ToggleableTimerForm';
 
-export default function App() {
-  return (
-    <View style={styles.appContainer}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Timers</Text>
+export default class App extends React.Component {
+  state = {
+    timers: [
+      {
+        title    : 'Mow the lawn',
+        project  : 'House Chores',
+        id       : uuiidv4(),
+        elapsed  : 5456099,
+        isRunning: true
+      },
+      {
+        title    : 'Bake squash',
+        project  : 'Kitchen Chores',
+        id       : uuiidv4(),
+        elapsed  : 1273998,
+        isRunning: false
+      }
+    ]
+  };
 
+  render() {
+    const { timers } = this.state;
+
+    return (
+      <View style={styles.appContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Timers</Text>
+        </View>
+        <ScrollView style={styles.timerList}>
+          <ToggleableTimerForm />
+          {
+            timers.map( ({title, project, id, elapsed, isRunning}) => (
+              <EditableTimer
+                key={id}
+                id={id}
+                title={title}
+                project={project}
+                elapsed={elapsed}
+                isRunning={isRunning}
+              />
+            ))
+          }
+        </ScrollView>
       </View>
-      <ScrollView style={styles.timerList}>
-        <ToggleableTimerForm isOpen={false}/>
-        <EditableTimer
-          id="1"
-          title="Mow the lawn"
-          project="House Chores"
-          elapsed="8986300"
-          isRunning
-        />
-        <EditableTimer
-          id="2"
-          title="Bake squash"
-          project="Kitchen Chores"
-          elapsed="3890985"
-          editFormOpen
-        />
-      </ScrollView>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -36,15 +57,15 @@ const styles = StyleSheet.create({
     flex: 1
   },
   titleContainer: {
-    paddingTop: 35,
-    paddingBottom: 15,
+    paddingTop       : 35,
+    paddingBottom    : 15,
     borderBottomWidth: 1,
     borderBottomColor: '#D6D7DA'
   },
   title         : {
-    fontSize: 18,
+    fontSize  : 18,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign : 'center'
   },
   timerList     : {
     paddingBottom: 15,
